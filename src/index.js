45ebdd872ef66742022e9a1d73f6b72854f29881
Boopin <36192860+boopin@users.noreply.github.com>
2025-03-25 14:30:40 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'; // Add this line
 import UpdatedSEOTool from './UpdatedSEOTool';
 ReactDOM.render(<UpdatedSEOTool />, document.getElementById('root'));
